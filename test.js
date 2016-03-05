52137a2a246cc67522cb9613d69f8776011144f0
ragingwind <ragingwind@gmail.com>
2016-03-05 19:13:41 +0900

    Using ava

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
-'use strict';
-var assert = require('assert');
-var sortSemver = require('./');
+import test from 'ava';
+import sortSemver from './';
 
 var tags = [
 	{ref: 'refs/tags/v1.0.2'},
@@ -21,38 +20,38 @@ var version = [
 	'v0.0.9'
 ];
 
-it('should returns array with sorted as asc', function () {
+test('should returns array with sorted as asc', t => {
 	var sorted = sortSemver.asc(version);
 
-	assert.strictEqual(sorted[0], 'v0.0.2');
-	assert.strictEqual(sorted[1], 'v0.0.9');
-	assert.strictEqual(sorted[2], 'v0.1.1');
-	assert.strictEqual(sorted[3], '1.1.1');
-	assert.strictEqual(sorted[4], '1.2.2');
-	assert.strictEqual(sorted[5], '2.0.1');
+	t.is(sorted[0], 'v0.0.2');
+	t.is(sorted[1], 'v0.0.9');
+	t.is(sorted[2], 'v0.1.1');
+	t.is(sorted[3], '1.1.1');
+	t.is(sorted[4], '1.2.2');
+	t.is(sorted[5], '2.0.1');
 });
 
-it('should returns array with sorted as desc', function () {
+test('should returns array with sorted as desc', t => {
 	var sorted = sortSemver.desc(version);
 
-	assert.strictEqual(sorted[5], 'v0.0.2');
-	assert.strictEqual(sorted[4], 'v0.0.9');
-	assert.strictEqual(sorted[3], 'v0.1.1');
-	assert.strictEqual(sorted[2], '1.1.1');
-	assert.strictEqual(sorted[1], '1.2.2');
-	assert.strictEqual(sorted[0], '2.0.1');
+	t.is(sorted[5], 'v0.0.2');
+	t.is(sorted[4], 'v0.0.9');
+	t.is(sorted[3], 'v0.1.1');
+	t.is(sorted[2], '1.1.1');
+	t.is(sorted[1], '1.2.2');
+	t.is(sorted[0], '2.0.1');
 });
 
-it('should returns array with sorted tag', function () {
+test('should returns array with sorted tag', t => {
 	var sorted = sortSemver.asc(tags.map(function (tag) {
 		return tag.ref;
 	}));
 
-	assert.ok(sorted[0].indexOf('v0.1.0') >= 0);
-	assert.ok(sorted[1].indexOf('v0.1.1') >= 0);
-	assert.ok(sorted[2].indexOf('v1.0.0') >= 0);
-	assert.ok(sorted[3].indexOf('v1.0.1') >= 0);
-	assert.ok(sorted[4].indexOf('v1.0.2') >= 0);
-	assert.ok(sorted[5].indexOf('v1.0.3') >= 0);
-	assert.ok(sorted[6].indexOf('v1.1.0') >= 0);
+	t.ok(sorted[0].indexOf('v0.1.0') >= 0);
+	t.ok(sorted[1].indexOf('v0.1.1') >= 0);
+	t.ok(sorted[2].indexOf('v1.0.0') >= 0);
+	t.ok(sorted[3].indexOf('v1.0.1') >= 0);
+	t.ok(sorted[4].indexOf('v1.0.2') >= 0);
+	t.ok(sorted[5].indexOf('v1.0.3') >= 0);
+	t.ok(sorted[6].indexOf('v1.1.0') >= 0);
 });
